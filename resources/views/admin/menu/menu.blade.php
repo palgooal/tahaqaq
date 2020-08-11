70bb7213d2552f50c8fcbcd84a948922a3b0087f
mostafa <63117117+DevMostafa1998@users.noreply.github.com>
2020-08-11 11:52:38 +0300

    menu

--- resources/views/admin/menu/menu.blade.php
+++ resources/views/admin/menu/menu.blade.php
@@ -96,17 +96,13 @@
                             <div class="modal-dialog" role="document">
                                <div class="modal-content">
                                   <div class="modal-header">
-                                     <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                                     <h5 class="modal-title" id="exampleModalLabel">تعديل القائمة</h5>
                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                      <span aria-hidden="true">&times;</span>
                                      </button>
                                   </div>
                                   <div class="modal-body">
-                                     <div class="card-header">
-                                        <h3 class="card-title">
-                                           تعديل القائمة
-                                        </h3>
-                                     </div>
+                                     
                                      <form method="post" action="/pg-admin/menus/{{$menu->id}}" accept-charset="UTF-8" class="form-horizontal" role="form" >
                                         <input type="hidden" name="_method" value="put">
                                         @csrf
@@ -125,14 +121,16 @@
                                            </div>
                                         </div>
                                         <div class="form-actions">
-                                           <button type="submit" class="btn blue" data-toggle="modal" data-target=".bd-example-modal-lg"{{$menu->id}}>تعديل</button>
-                                           <a href="/pg-admin/menus" type="button" class="btn default">الغاء</a>
+                                           <button type="submit" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg"{{$menu->id}}>تعديل</button>
+                                           {{-- <a href="/pg-admin/menus" type="button" class="btn default">الغاء</a> --}}
+                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">الغاء</button>
+
                                         </div>
                                      </form>
                                   </div>
                                   <div class="modal-footer">
-                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                     <button type="button" class="btn btn-primary">Send message</button>
+                                     {{-- <button type="button" class="btn btn-secondary" data-dismiss="modal">الغاء</button> --}}
+                                     {{-- <button type="button" class="btn btn-primary">Send message</button> --}}
                                   </div>
                                </div>
                             </div>
