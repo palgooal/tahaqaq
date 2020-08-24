f25e44558addf1a66361e323bab5dfa4a8bb34ac
Pal Goal <palgooal@gmail.com>
2020-08-24 21:40:09 +0300

    comment

---
+++ database/factories/CommentsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Comments;
+use Faker\Generator as Faker;
+
+$factory->define(Comments::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
