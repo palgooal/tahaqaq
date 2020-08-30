b227b3cb7176cd49f5c4093b0d9465eee7b7cb41
mostafa <63117117+DevMostafa1998@users.noreply.github.com>
2020-08-30 15:12:00 +0300

    تعديل على تصميمoneTemplate

--- resources/views/getOneTemplate.blade.php
+++ resources/views/getOneTemplate.blade.php
@@ -1,18 +1,54 @@
 @extends('layouts.app')
 @section('content')
-<section id="getOnetemplate">
-    <div class="container text-center">
-            <div class="cardTemplate">
-                <div class="header" >
+<section id="getOnetemplate" dir="rtl">
+    <div class="header_section text-center">
+        <div class="row">
+            <div class="header">
+                <div class="col-sm-12">
                     <h3>{{$templateOne->title_ar}}</h3>
-                    <div class="img" style="background-image: url('images/{{$templateOne->image_url}}')">
-                        <div class="previoLive" style="">
-                            <a href="{{$templateOne->preview_url}}" class="btnPreviw">
-                                 <button type="button" class="btn btn-light btn-lg">View Live Preview</button>
-                            </a>
-                        </div>
+                </div>
+                <div class="col-md-6 col-sm-12 left">
+                    <div class="" style="">
+                        <a href="{{$templateOne->preview_url}}" class="">
+                             <button type="button" class="btn btn-light btn-lg">شاهد تاموقع</button>
+                        </a>
                     </div>
-                    {{-- <img src="images/{{$templateOne->image_url}}" width="80%" height="100%"> --}}
+                </div>
+                <div class="col-md-6 col-sm-12 right">
+                    <div class="" style="">
+                        <a href="" class="">
+                             <button type="button" class="btn btn-light btn-lg">اختر الموقع</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container text-center" >
+
+        <div class="row cardTemplate">
+            <div class="col-sm-12 right">
+                <p class="">التصنيف: {{$templateOne->category_id}}</p>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-12 header">
+                <div class="img" style="background-image: url('images/{{$templateOne->image_url}}')">
+
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 right">
+                <div class="details">
+                    {!!$templateOne->details_ar!!}
+                </div>
+            </div>
+        </div>
+            {{-- <div class="cardTemplate">
+                <div class="header" >
+
+                    <div class="img" style="background-image: url('images/{{$templateOne->image_url}}')">
+
+                    </div>
+                    <img src="images/{{$templateOne->image_url}}" width="80%" height="100%">
                 </div>
                 <div class="card-body text-right">
                     <div class="text-center" id="btnDownLivePreviw">
@@ -25,7 +61,7 @@
                         {!!$templateOne->details_ar!!}
                     </div>
                 </div>
-            </div>
+            </div> --}}
     </div>
 </section>
 
