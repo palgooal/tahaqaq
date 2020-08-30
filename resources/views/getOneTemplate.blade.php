3a36e9607d329150075e5c112bcae14c7253db11
mostafa <63117117+DevMostafa1998@users.noreply.github.com>
2020-08-30 17:02:42 +0300

    تصميم  مميزات المواقع لدينا في صفحة getOneTemplate

--- resources/views/getOneTemplate.blade.php
+++ resources/views/getOneTemplate.blade.php
@@ -42,26 +42,121 @@
                 </div>
             </div>
         </div>
-            {{-- <div class="cardTemplate">
-                <div class="header" >
 
-                    <div class="img" style="background-image: url('images/{{$templateOne->image_url}}')">
 
-                    </div>
-                    <img src="images/{{$templateOne->image_url}}" width="80%" height="100%">
+    </div>
+</section>
+
+<section id="Our_advantages_section">
+    <div class="container text-center">
+        <div class="Our_advantages">
+            <div class="row">
+                <div class="col-sm-12 title_ourAd">
+                    <h3>مميزات المواقع لدينا</h3>
                 </div>
-                <div class="card-body text-right">
-                    <div class="text-center" id="btnDownLivePreviw">
-                        <a href="{{$templateOne->preview_url}}" class="">
-                            <button type="button" class="btn btn-success btn-lg">View Live Preview</button>
-                       </a>
-                    </div>
-                    <p>التصنيف: {{$templateOne->category_id}}</p>
-                    <div class="details">
-                        {!!$templateOne->details_ar!!}
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/free-delivery.png">
+                        </div>
+                        <div class="body">
+                            <p>انه مجاني</p>
+                        </div>
                     </div>
                 </div>
-            </div> --}}
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/customer-service.png">
+                        </div>
+                        <div class="body">
+                            <p>دعم فني مميز على مدار الساعة</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/settings.png">
+                        </div>
+                        <div class="body">
+                            <p>سهل الاستخدام والتحكم</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/security.png">
+                        </div>
+                        <div class="body">
+                            <p>آمن ومحصن</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/browser.png">
+                        </div>
+                        <div class="body">
+                            <p>جاهزة للإستخدام</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/translation.png">
+                        </div>
+                        <div class="body">
+                            <p>يدعم العديد من اللغات</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/medal.png">
+                        </div>
+                        <div class="body">
+                            <p>مرن و عالي الجودة</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/analysis.png">
+                        </div>
+                        <div class="body">
+                            <p>محسن لمحركات البحث</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-12">
+                    <div class="cardAd">
+                        <div class="header">
+                            <img src="https://matjaree.com/arabic-website-templates/wp-content/uploads/2018/02/responsive-1.png">
+                        </div>
+                        <div class="body">
+                            <p>تصاميم متجاوبة</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
     </div>
 </section>
 
