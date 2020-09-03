08b77bc8c5c7da5e0cccf4fc07fb3d3799b12f05
Pal Goal <palgooal@gmail.com>
2020-09-03 19:47:51 +0300

    template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,7 +3,7 @@
 @include('partials.InternalNavpar')
 <a href="/" class="re-index"> <img src="img/arrow-right.png">الرجوع الى الرئيسية</a>
 <div class="section-title wow fadeInDown">قوالب Wordpress</div>
-<div class="container">
+{{-- <div class="container"> --}}
 {{-- <section id="templateView">
     <div class="row text-center">
         <div class="col-lg-1 colCategory">
@@ -43,48 +43,31 @@
     </div>
 </section> --}}
 <!--section one -->
-
-
-<div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 text-right"><div id="sidebar-main" class="sidebar sidebar-default sidebar-separate">
-<div class="sidebar-category sidebar-default">
-    <div class="category-title">
-        <span>جميع التصنيفات</span>
-    </div>
-    <div class="category-content">
-        <ul id="fruits-nav" class="nav flex-column">
-            @foreach ($categoris as $categoris)
-            <li class="nav-item">
-                <a href="{{$categoris->id}}" class="nav-link">{{$categoris->text_ar}}</a>
-            </li>
-            @endforeach
-        </ul>
-    </div>
-</div>
-</div></div>
-            <div class="col-md-8">
-                <div class="row product-list">
-                    @foreach ($templateAll as $templateAll)
-                    <div class="col-sm-6 col-md-4 product-item">
-                        <div class="product-container">
-                            <div class="row">
-                                <div class="col-md-12"><a class="product-image" href="#"><img src="images/{{$templateAll->image_url}}"></a></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12">
-                                    <h2 class="text-center"><a href="#">{{$templateAll->title_ar}} </a></h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-
-                </div>
-            </div>
+<section class="template-header">
+    <div id="pagina">
+        <div class="barra-menu">
+            <!-- Boton Desplegar/Replegar menú -->	<a href="#" id="btn-drop" class="btn-menu aln-centro">Navegacion</a>
+            <!-- Menú de navegación -->
+            <ul id="submenu" class="lista-menu aln-centro">
+                @foreach ($categoris as $categoris)
+                <li><a href="#">{{$categoris->text_ar}}</a></li>
+                @endforeach
+            </ul>
         </div>
     </div>
-</div>
+</section>
+<section class="template">
+    @foreach ($templateAll as $templateAll)
+    <a href=""><div class="wrapper">
+        <div class="shadow"></div>
+        <h2 class="animated bounceInLeft">{{$templateAll->title_ar}}</h2>
+        <p class="animated bounceInLeft">{{$templateAll->small_details_ar}}</p>
+       <img alt=""  src="images/{{$templateAll->image_url}}">
+      </div></a>
+      @endforeach
+
+
+    </section>
+
 
 @endsection
