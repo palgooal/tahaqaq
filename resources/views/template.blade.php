d26a7080b6b0fbc125688fcda0ecdcffcd396bab
Pal Goal <palgooal@gmail.com>
2020-09-03 20:00:10 +0300

    template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -58,12 +58,12 @@
 </section>
 <section class="template">
     @foreach ($templateAll as $templateAll)
-    <a href=""><div class="wrapper">
-        <div class="shadow"></div>
+    <div class="wrapper">
+        <a href="#"> <div class="shadow"></div>
         <h2 class="animated bounceInLeft">{{$templateAll->title_ar}}</h2>
         <p class="animated bounceInLeft">{{$templateAll->small_details_ar}}</p>
-       <img alt=""  src="images/{{$templateAll->image_url}}">
-      </div></a>
+        <img alt=""  src="images/{{$templateAll->image_url}}"></a>
+      </div>
       @endforeach
 
 
