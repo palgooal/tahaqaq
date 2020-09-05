b3f4e57cedd40c53076463b33b7d8cd7927a5662
mostafa <63117117+DevMostafa1998@users.noreply.github.com>
2020-09-05 22:23:46 +0300

    template selector

---
+++ resources/views/template/partials/templateSelector.blade.php
@@ -0,0 +1,42 @@
+<section class="template">
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="row scrolDiv"  id="contintTemplate">
+                @isset($templateAll)
+                    @foreach ($templateAll as $templateAll)
+                        <div class="col-sm-12">
+                            <a href="/template/{{$templateAll->id}}">
+                                <div class="wrapper">
+                                    <div class="shadow"></div>
+                                    <h2 class="animated bounceInLeft">{{$templateAll->title_ar}}</h2>
+                                    <p class="animated bounceInLeft">{{$templateAll->small_details_ar}}</p>
+                                    <img alt="" src="images/{{$templateAll->image_url}}">
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                @endisset
+            </div>
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-12 detailsTemplateSelector">
+            <div class="imgTemplate" width='200px' height='100px'>
+                <img src="images/images/sb-admin-pro-html.jpg" width="100%" height="100%">
+            </div>
+            <input type="button" class="btn btn-primary" value="تفاصيل اضافية">
+            <input type="button" class="btn btn-primary" value="تصفح النموذج">
+            <h2 class="wow fadeInDown" style="visibility: visible; animation-name: fadeInDown;">تصميم معاصر ولطيف وخفيف</h2>
+            <p style="text-align: justify">
+                    تصميم معاصر ولطيف وخفيف تصميم معاصر ولطيف وخفيف تصميم معاصر ولطيف وخفيف
+
+                    تصميم معاصر ولطيف وخفيف
+
+                    تصميم معاصر ولطيف وخفيف
+
+                    تصميم معاصر ولطيف وخفيف تصميم معاصر ولطيف وخفيف تصميم معاصر ولطيف وخفيف تصميم معاصر ولطيف وخفيف
+
+                    تصميم معاصر ولطيف وخفيف تصميم معاصر ولطيف وخفيف
+            </p>
+        </div>
+    </div>
+
+</section>
