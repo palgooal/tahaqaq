3845465d6509cec036949c0c2b23ce77e9213e47
mostafa <63117117+DevMostafa1998@users.noreply.github.com>
2020-09-08 11:53:04 +0300

    slider blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,61 @@
 @extends('layouts.app') @section('content') @include('partials.InternalNavpar')
 <!--section one -->
-<section class="blogs wow fadeIn">
+<section class="blogs wow fadeIn" id="blogId">
 	<div class="container">
+
+        <!-- Carousel container -->
+
+<div id="my-pics" class="carousel slide" data-ride="carousel" style="min-height:100px;margin:auto;">
+
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <?php $counterBlogerSlider = 0;?>
+        @foreach ($blogs as $blogsSliderPics)
+            @if ($blogsSliderPics->pin_to_slider == 1)
+                <li data-target="#my-pics" data-slide-to="{{$counterBlogerSlider}}" class="{{$counterBlogerSlider == 1 ? 'active':''}}"></li>
+                <?php $counterBlogerSlider++ ?>
+            @endif
+        @endforeach
+        {{-- {{$counterBlogerSlider}} --}}
+
+    </ol>
+
+    <!-- Content -->
+    <div class="carousel-inner" role="listbox">
+
+    <!-- Slide 1 -->
+    <?php $counterBlogerSlider = 0;?>
+    @foreach ($blogs as $blogsSliderimg)
+        @if ($blogsSliderimg->pin_to_slider == 1)
+            <div class="item {{$counterBlogerSlider == 0?'active':''}}">
+                <img src="images/{{$blogsSliderimg->image}}" alt="Sunset over beach">
+                <div class="carousel-caption">
+                    <h3>Boracay</h3>
+                    <p>White Sand Beach.</p>
+                </div>
+            </div>
+            <?php $counterBlogerSlider++ ?>
+        @endif
+    @endforeach
+
+
+    </div>
+
+    <!-- Previous/Next controls -->
+    <a class="left carousel-control" href="#my-pics" role="button" data-slide="prev">
+    <span class="icon-prev" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#my-pics" role="button" data-slide="next">
+    <span class="icon-next" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+    </a>
+
+</div>
+
+    </div>
+
+
 		<a href="#" class="re-index">
 			<img src="img/arrow-right.png">الرجوع الى الرئيسية</a>
 		<div class="section-title wow fadeInDown">المدونة</div>
@@ -53,4 +107,15 @@
 	</div>
 </section>
 <!--</section>-->
+<script>
+    // Initialize tooltip component
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    // Initialize popover component
+    $(function () {
+      $('[data-toggle="popover"]').popover()
+    })
+    </script>
 <!--end section-->@endsection
